b546ed44863b1b179612ae5b399716a1f1a3dd2b
axel.latour <axel.latour@cgi.com>
2017-04-03 15:03:08 +0200

    Remove useless pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { OfferPage } from '../pages/offer/offer';
 import { OffersListPage } from '../pages/offers-list/offers-list';
@@ -20,9 +17,6 @@ import { SuperTabsModule } from 'ionic2-super-tabs';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     TabsPage,
     OfferPage,
     OffersListPage,
@@ -36,9 +30,6 @@ import { SuperTabsModule } from 'ionic2-super-tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
     OfferPage,
     OffersListPage,
     ServicePage,
