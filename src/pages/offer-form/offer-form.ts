64403dd73778b4e20ea5ee0729f52fb710eae6b7
axel.latour <axel.latour@cgi.com>
2017-04-24 14:26:42 +0200

    Minor fix

--- src/pages/offer-form/offer-form.ts
+++ src/pages/offer-form/offer-form.ts
@@ -106,9 +106,9 @@ export class OfferFormPage implements OnInit {
   }
 
   submitForm(): void {
-    //console.log(this.offerForm);
-    //console.log(this.offerForm.value);
-    //console.log(this.features);
+    console.log(this.offerForm);
+    console.log(this.offerForm.value);
+    console.log(this.features);
     this.navCtrl.push(PaymentPage);
 
   }
