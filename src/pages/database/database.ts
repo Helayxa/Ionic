d608674c83e9161c5f955d2d5c57ad66a9a916c2
clemscode <ruffin.cle@gmail.com>
2017-04-24 15:57:19 +0200

    first database stable version

--- src/pages/database/database.ts
+++ src/pages/database/database.ts
@@ -15,32 +15,18 @@ export class DatabasePage {
   }
 
   ionViewDidLoad() {
-    this.databaseService.dropTables();
     this.databaseService.createTablesIfNotExist();
+    this.databaseService.subscribe(['Clement', 'Ruffin', 'ruffin.Cle@gmail.Com', 21], 2, [4]).then(data => {
+      console.log(data);
+    }).catch(error => {
+      console.log(error);
+    });
   }
 
   select() {
-    this.databaseService.sql("SELECT * FROM client", []).then(data => {
-      let users: any[];
-      this.databaseService.log("Data length", data.rows);
-      for(let i=0; i<data.rows.length; i++) {
-        users.push({
-          clientSurname: data.rows.item(i).clientSurname,
-          clientName: data.rows.item(i).clientName
-        });
-      }
-      this.databaseService.log("Select OK :", users);
-    }).catch(error => {
-      this.databaseService.log("Select error : ", error);
-    });
+
   }
 
-  testInsert() {
-    this.databaseService.sql("INSERT INTO client(clientSurname, clientName, clientMail, clientAge) VALUES('Ruffin', 'Clément', 'ruffin.cle@gmail.com', 21)", []).then(data => {
-      this.databaseService.log("Insert OK", data);
-    }).catch(error => {
-      this.databaseService.log("Insert Error", error);
-    });
-  }
+
 
 }
