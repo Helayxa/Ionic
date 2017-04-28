593575307c02b472a70ce39447c42da8285e5b2f
clement1402 <lgkg850>
2017-04-28 15:38:54 +0200

    Admin page

--- src/pages/subscription-list/subscription-list.ts
+++ src/pages/subscription-list/subscription-list.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+
+import { Chart } from 'chart.js';
 
 /*
   Generated class for the SubscriptionList page.
@@ -13,13 +15,37 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class SubscriptionListPage {
 
+  @ViewChild('paymentWayCanvas') paymentWayCanvas;
+  paymentWayChart: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SubscriptionListPage');
-  }
-
-  informations(){
-    console.log('Hello !')
+  ionViewDidLoad()
+  {
+    this.paymentWayChart = new Chart(this.paymentWayCanvas.nativeElement, {
+      type: 'doughnut',
+      data: {
+        labels: ['PayPal', 'Carte de crédit', 'Virement bancaire'],
+        datasets: [{
+          label: 'Moyens de paiement',
+          data: ["10", "50", "40"],
+          backgroundColor: [
+            'rgb(244,67,54)',
+            'rgb(33,150,243)',
+            'rgb(238,238,238)'
+          ],
+          hoverBackgroundColor: [
+            '#d32f2f',
+            'rgb(25,118,210)',
+            'rgb(238,238,238)'
+          ]
+        }]
+      },
+      options: {
+        legend: {
+          position: 'bottom'
+        }
+      }
+    });
   }
 }
