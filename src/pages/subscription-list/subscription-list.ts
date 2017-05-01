08c59b2086899503b38924aeaa09ac4eaa18b751
Kaolyne <lemaire.pauline08@gmail.com>
2017-05-01 10:31:30 +0200

    chart 0.1

--- src/pages/subscription-list/subscription-list.ts
+++ src/pages/subscription-list/subscription-list.ts
@@ -18,6 +18,12 @@ export class SubscriptionListPage {
   @ViewChild('paymentWayCanvas') paymentWayCanvas;
   paymentWayChart: any;
 
+  @ViewChild('subscriptionDetailsCanvas') subscriptionDetailsCanvas;
+  suscriptionDetailsChart: any;
+
+  @ViewChild('tendencyCanvas') tendencyCanvas;
+  tendencyChart: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad()
@@ -28,22 +34,92 @@ export class SubscriptionListPage {
         labels: ['PayPal', 'Carte de crédit', 'Virement bancaire'],
         datasets: [{
           label: 'Moyens de paiement',
-          data: ["10", "50", "40"],
+          data: [10, 50, 40],
           backgroundColor: [
-            'rgb(244,67,54)',
-            'rgb(33,150,243)',
-            'rgb(238,238,238)'
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
           ],
           hoverBackgroundColor: [
-            '#d32f2f',
-            'rgb(25,118,210)',
-            'rgb(238,238,238)'
+            "#FF6384",
+            "#36A2EB",
+            "#FFCE56"
           ]
         }]
       },
       options: {
         legend: {
           position: 'bottom'
+        }
+      }
+    });
+    this.suscriptionDetailsChart = new Chart(this.subscriptionDetailsCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['Offre 1', 'Offre 2', 'Offre 3', 'Offre 4'],
+        datasets: [{
+          label: 'Nombre de suscription par offre',
+          data: [10, 50, 25, 15],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+          ]
+        }]
+      },
+      options: {
+        legend: {
+          position: 'bottom'
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+    this.tendencyChart = new Chart(this.tendencyCanvas.nativeElement, {
+      type: 'line',
+      data: {
+        labels:['Free', 'SFR', 'Orange', 'Canal+'],
+        datasets: [{
+          label:'nombre de suscription',
+          data: [{
+              y: 120,
+              x: 1
+            },
+            {
+              y: 52,
+              x: 2
+            },
+            {
+              y: 684,
+              x: 3
+            },
+            {
+              y: 452,
+              x: 4
+            }
+          ],
+          backgroundColor:"#FF6384",
+          hoverBackgroundColor: "#FF6384"
+        }]
+      },
+      options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
         }
       }
     });
