f95daaab03046acab7bb8458ae5f56efcd0cad3a
Kaolyne <lemaire.pauline08@gmail.com>
2017-05-08 16:53:59 +0200

    icon + splash screen

--- src/pages/subscription-list/subscription-list.ts
+++ src/pages/subscription-list/subscription-list.ts
@@ -22,8 +22,8 @@ export class SubscriptionListPage {
   @ViewChild('subscriptionDetailsCanvas') subscriptionDetailsCanvas;
   suscriptionDetailsChart: any;
 
-  @ViewChild('tendencyCanvas') tendencyCanvas;
-  tendencyChart: any;
+  @ViewChild('moneyByOffersCanvas') moneyByOffersCanvas;
+  moneyByOffersChart: any;
 
   private hash: string;
   private json: any;
@@ -40,17 +40,20 @@ export class SubscriptionListPage {
   }
 
   ionViewWillEnter() {
+    console.log(this.json);
     for(let i=0; i<this.json.offers.length; i++){
       this.s_offersArray[i] = this.json.offers[i].title;
       this.n_offersArray[i] = 0;
     }
 
     this.databaseService.findAllSubscriptions(this.hash).then(data => {
+      console.log(data);
       this.nbSouscription = data.length;
       for(let i=0; i<data.length; i++){
         this.n_offersArray[data[i].offerId] += 1;
       }
       this.generateDetailsChart();
+      this.generateMoneybyOffersChart();
     }).catch(error => {
       console.log(error);
     });
@@ -99,6 +102,49 @@ export class SubscriptionListPage {
     });
   }
 
+  generateMoneybyOffersChart(): void{
+    this.moneyByOffersChart = new Chart(this.moneyByOffersCanvas.nativeElement, {
+      type: 'horizontalBar',
+      data: {
+        labels: this.s_offersArray,
+        datasets: [{
+          data: this.n_offersArray,
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          hoverBackgroundColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+          ]
+        }]
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            ticks: {
+              beginAtZero: true,
+              stepSize: 1
+            }
+          }]
+        },
+        title:{
+          display: true,
+          text: 'Gain par offres'
+        },
+        legend:{
+          display: false
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    });
+  }
+
   ionViewDidLoad()
   {
     this.paymentWayChart = new Chart(this.paymentWayCanvas.nativeElement, {
@@ -123,42 +169,6 @@ export class SubscriptionListPage {
       options: {
         legend: {
           position: 'bottom'
-        }
-      }
-    });
-    this.tendencyChart = new Chart(this.tendencyCanvas.nativeElement, {
-      type: 'line',
-      data: {
-        labels:['Free', 'SFR', 'Orange', 'Canal+'],
-        datasets: [{
-          label:'nombre de suscription',
-          data: [{
-              y: 120,
-              x: 1
-            },
-            {
-              y: 52,
-              x: 2
-            },
-            {
-              y: 684,
-              x: 3
-            },
-            {
-              y: 452,
-              x: 4
-            }
-          ],
-          backgroundColor:"#FF6384",
-          hoverBackgroundColor: "#FF6384"
-        }]
-      },
-      options: {
-        scales: {
-            xAxes: [{
-                type: 'linear',
-                position: 'bottom'
-            }]
         }
       }
     });
